86856db501dc323649fb0532ba3c788b4ae65150
Simon <simgus1234@gmail.com>
2023-11-21 19:43:21 +0100

    Made buttons work

--- rating.js
+++ rating.js
@@ -10,67 +10,105 @@ const ratingSystem = (() => {
             document.querySelector("#option3"),
             document.querySelector("#option4"),
             document.querySelector("#option5")
-        ]
+        ];
 
         optionArray.forEach(option => {
-            option.addEventListener("click", () => {
+            option.addEventListener("click",handleClick)
+        })
 
-                optionArray.forEach(otherOption => {
-                    otherOption.classList.remove("active")
-                })
 
-                option.classList.add("active")
+        function handleClick(){
+            optionArray.forEach(otherOption => {
+                otherOption.classList.remove("active");
             })
-        });
-    })();
+
+            this.classList.add("active")
+
+
+            getValue()
+        }
+
+    });
 
 
 
 
     const getValue = () => {
 
+        let optionArray = [
+            document.querySelector("#option1"),
+            document.querySelector("#option2"),
+            document.querySelector("#option3"),
+            document.querySelector("#option4"),
+            document.querySelector("#option5")
+        ]
+
         let value = "";
 
-        if(optionArray[0].classList.contains(".active")){
-            value = "1";
-        }else if (option2.classList.contains(".active")){
-            option1.classList.remove(".active")
-            option3.classList.remove(".active")
-            option4.classList.remove(".active")
-            option5.classList.remove(".active")
-            value = "2";
-        }else if(option3.classList.contains(".active")){
-            option2.classList.remove(".active")
-            option1.classList.remove(".active")
-            option4.classList.remove(".active")
-            option5.classList.remove(".active")
-            value = "3";
-        }else if(option4.classList.contains(".active")){
-            option2.classList.remove(".active")
-            option3.classList.remove(".active")
-            option1.classList.remove(".active")
-            option5.classList.remove(".active")
-            value = "4";
-        }else if(option5.classList.contains(".active")){
-            option2.classList.remove(".active")
-            option3.classList.remove(".active")
-            option4.classList.remove(".active")
-            option1.classList.remove(".active")
-            value = "5";
+        let activeOption = optionArray.find(option => option.classList.contains("active"));
+
+        if (activeOption){
+            switch(activeOption.id){
+                case "option1":
+                    value = "1";
+                    console.log(value);
+                break;
+                case "option2":
+                    value="2";
+                    console.log(value);
+                break
+                case "option3":
+                    value="3";
+                    console.log(value);
+                break
+                case "option4":
+                    value="4";
+                    console.log(value);
+                break
+                case "option5":
+                    value = "5";
+                    console.log(value)
+                break
+            }
+
+            return value
         }
 
 
-        return value
+
+
+
+
+    
+    }
+
+
+
+    const buttonPressed = () => {
+
+
+        let button = document.getElementById("#ratingbutton");
+        let msgBox = document.querySelector("succesmsg");
+
+        button.addEventListener("click", displayUpdater)
+
+        function displayUpdater(){
+
+            msgBox.style.display = "flex";
+
+        }
+
 
     }
 
 
-    const updateValue = () => {
 
-    }
+    const launchStuff = (() => {
 
-    const changeActive = () => {
+        buttonScanner();
+        buttonPressed();
+        getValue();
 
-    }
+    })();
 
 })();
